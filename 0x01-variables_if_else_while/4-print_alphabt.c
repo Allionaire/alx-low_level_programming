dfb600c3d6d7ae65da5ac652ed84dda3aee7885f
Allionaire <alinafemlenga2@gmail.com>
2023-07-17 15:18:08 -0700

    task4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,18 +3,17 @@
 #include <string.h>
 
 /**
- * Action - Shows the alphabet without q and e.
+ * main - Entry
+ * Shows the alphabet without q and e.
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
 	int i;
-
 	for (i = 97; i < 123; i++)
 	{
 		if (i != 101 && i != 113)
-	
 		{		
 			putchar(i);
 		}
