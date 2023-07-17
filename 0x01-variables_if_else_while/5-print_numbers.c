9ae79616abcf8eecd0c855e818b778ed17b78af2
Allionaire <alinafemlenga2@gmail.com>
2023-07-17 15:55:07 -0700

    task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - Entry 
+ * Shows numbers from 0 to 9.
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+	
+{
+
+	int i;
+
+	for (i = 0; i < 10; i++)
+	printf("%d", i);
+	printf("\n");
+	return (0);
+
+}
+
