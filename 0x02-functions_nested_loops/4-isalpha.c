ab237b68ead3cfacceeab43ab9f62cd1f4506736
Allionaire <alinafemlenga2@gmail.com>
2023-07-19 14:08:56 -0700

    4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
 /**
- * _isalpha - checks for alphabetic char
- * @c: the char to be checked
+ * _isalpha - displays alphabetic order
+ * @c: alpabet to check for
  * Return: 1 if c is a letter, 0 otherwise
  */
 
 int _isalpha(int c)
-
 {
 	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
-}
 
+}
